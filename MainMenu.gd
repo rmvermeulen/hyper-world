20e48f4b30859e7179f161729cf0e9f3e73912a8
Rasmus Vermeulen <rasmus_vermeulen@tutanota.com>
2020-07-20 13:41:20 +0200

    hyper world: add some folders

---
+++ MainMenu.gd
@@ -0,0 +1,17 @@
+extends Control
+
+
+func _on_Button2D_pressed():
+	get_tree().change_scene("res://cube3_map_2d/MainScene.tscn")
+
+
+func _on_Button3D_pressed():
+	get_tree().change_scene("res://cube3_map_3d/MainScene.tscn")
+
+
+func _on_Button4D_pressed():
+	get_tree().change_scene("res://cube4_map_3d/MainScene.tscn")
+
+
+func _on_ButtonExit_pressed():
+	get_tree().quit()
