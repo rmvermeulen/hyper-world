e473bff50f2cd2f278609e0eb866520f0963a1d4
Rasmus Vermeulen <vermeulen.rasmus@gmail.com>
2020-07-27 11:20:09 +0200

    hyper world: fix lib errors

--- tesseract/Hyper.gd
+++ tesseract/Hyper.gd
@@ -19,8 +19,8 @@ class V4:
 			total += (v4[i] * v4[i])
 		return total
 
-	static func length() -> float:
-		return sqrt(length_squared())
+	static func length(v4: Array) -> float:
+		return sqrt(length_squared(v4))
 
 	static func project(v4: Array) -> Vector3:
 		return Vector3(v4[0], v4[1], v4[2]) * ((1.0 - (v4[3])) + length())
@@ -30,5 +30,5 @@ class V4:
 	# 	# todo
 	# 	return v
 
-	static func to_string(v4: Array) -> String:
+	static func as_string(v4: Array) -> String:
 		return "Vector4(%.2f, %.2f, %.2f, %.2f)" % v4
