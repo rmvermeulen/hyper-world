51e0ba549eb0e7d39fed0de1585751afde8db45f
Rasmus Vermeulen <vermeulen.rasmus@gmail.com>
2020-08-02 01:15:26 +0200

    Update galaxy scene

--- components/Galaxy.gd
+++ components/Galaxy.gd
@@ -4,5 +4,9 @@ extends MeshInstance
 export (float) var seconds_per_rotation := 120.0
 
 
+func _ready():
+	rotation.z = 0
+
+
 func _process(delta):
 	rotation.z += delta * (TAU / seconds_per_rotation)
